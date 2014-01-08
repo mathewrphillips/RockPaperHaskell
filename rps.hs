ec051cd9aeb8831aa1e3574ecb953486769bb751
Matt Phillips <mathewrphillips@gmail.com>
2014-01-07 23:11:58 -0600

    renamed some methods to make the meaning clearer. Simplified isGameOver

--- rps.hs
+++ rps.hs
@@ -2,6 +2,8 @@ import System.Random
 import Control.Monad.State
 
 data Hand = Rock | Scissors | Paper  deriving(Show,Eq)
+data Result = Win | Lose | Tie deriving(Show)
+type Score = (Int,Int)
 
 fight :: Hand -> Hand -> Result
 fight h1 h2 | h1 == h2 = Tie
@@ -9,9 +11,6 @@ fight h1 h2 | h1 == h2 = Tie
             | h1 == Paper && h2 == Rock = Win
             | h1 == Scissors && h2 == Paper = Win
             | otherwise = Lose
-
-type Score = (Int,Int)
-data Result = Win | Lose | Tie deriving(Show)
 
 updateScore :: Result -> StateT Score IO Result 
 updateScore r = do
@@ -24,12 +23,12 @@ updateScore r = do
 
 playR :: StateT Score IO ()
 playR = do
-    io $ putStrLn "(r)ock, (p)aper, or (s)cissors"
-    r <- io $ liftM2 fight getrps genrps
-    io $ print r
+    liftIO $ putStrLn "(r)ock, (p)aper, or (s)cissors"
+    r <- liftIO $ liftM2 fight getHand genHand
+    liftIO $ print r
     updateScore r
     s <- get
-    io $ putStrLn ((show . fst) s ++ " - " ++ (show . snd )s)
+    liftIO $ putStrLn ((show . fst) s ++ " - " ++ (show . snd )s)
     if isGameOver s then endGame s else playR 
 
 endGame :: Score -> StateT Score IO () 
@@ -37,24 +36,25 @@ endGame (3,_) = io $ putStrLn "You Win!"
 endGame (_,3) = io $ putStrLn "Sorry You Lost"
 
 isGameOver :: Score -> Bool
-isGameOver (x,y) | x == 3 || y == 3 = True
-                 | otherwise = False
+isGameOver (x,y) = x == 3 || y == 3
 
 io :: IO a -> StateT Score IO a
 io = liftIO
 
-genrps :: IO Hand
-genrps = fmap rps rand
+genHand :: IO Hand
+genHand = fmap rps rand
     where rps 1 = Rock
           rps 2 = Paper
           rps _ = Scissors
           rand = randomRIO (1,3) :: IO Int
 
-getrps = fmap rps getLine
+getHand :: IO Hand
+getHand = fmap rps getLine
     where rps "r" = Rock
           rps "p" = Paper
           rps "s" = Scissors
           rps _ = error "invalid choice"
 
+main :: IO ()
 main = void $ runStateT playR (0,0)
     
